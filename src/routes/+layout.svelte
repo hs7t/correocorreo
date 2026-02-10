26282ecf2cb74423bba80cf7218f6cad61ecd24c
Alí Sinisterra <haidarconh@gmail.com>
2026-02-10 18:41:48 -0500

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import favicon from '$lib/assets/favicon.svg';
+
+	let { children } = $props();
+</script>
+
+<link rel="preconnect" href="https://fonts.googleapis.com" />
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+<link
+	href="https://fonts.googleapis.com/css2?family=Crimson+Text:ital,wght@0,400;0,600;0,700;1,400;1,600;1,700&display=swap"
+	rel="stylesheet"
+/>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+{@render children()}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+	import Form from '$lib/components/form.svelte';
+	import '$lib/style.css';
+
+	import love from '$lib/assets/love.svg';
+</script>
+
+<main>
+	<header>
+		<img class="logo-img" src={love} alt="Un dibujo de una flecha atravezando una carta." />
+		<h1>Correo Correo</h1>
+	</header>
+	<Form></Form>
+</main>
+
+<style>
+	:global(body) {
+		display: flex;
+		width: 100vw;
+		height: 100vh;
+
+		justify-content: center;
+		align-content: center;
+	}
+
+	.logo-img {
+		width: 6ch;
+	}
+
+	h1 {
+		font-size: inherit;
+		font-style: italic;
+		font-weight: inherit;
+	}
+
+	header {
+		opacity: 70%;
+	}
+
+	main {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+
+		gap: 0.5em;
+		width: 70%;
+		min-width: 0;
+	}
+</style>

1061cb8c3a7156f9d8f649be73a43043567ee187
Alí Sinisterra <haidarconh@gmail.com>
2026-02-10 18:45:33 -0500

    add crossorigin attr

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <link rel="preconnect" href="https://fonts.googleapis.com" />
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
 <link
 	href="https://fonts.googleapis.com/css2?family=Crimson+Text:ital,wght@0,400;0,600;0,700;1,400;1,600;1,700&display=swap"
 	rel="stylesheet"
